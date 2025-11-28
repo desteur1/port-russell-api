3a923f8828a9c66ed49432027000ca2dfd8e35ec
Desteur Fall <shmurdanoah@gmail.come>
2025-11-28 10:19:56 +0100

    Create Mongoose Models: Users, Catway, Reservation

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 - npm install -g express-generator ( pour le squelette d'application)
 - npm install mongoose ( connexion à MongoDB)
 - npm install dotenv (pour protéger la connexion MongoDB)
+- npm install mongoose ( pour connecter l'API à MongoDB et définir les modèles)
+- npm install nodemon (redémarre automatiquement le serveur à chaque modification)
+- npm install bcrypt (pour hasher les mots de passe des utilisateurs)
 
 #### TP
 
---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// Middleware pour hasher le mot de passe avant de sauvegarder l'utilisateur
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return; // si pas modifié, on sort
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+module.exports = mongoose.model("User", userSchema);
